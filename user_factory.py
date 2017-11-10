ed1a70cec4e7f9da6dff1e346462e9dee27e3423
Sam Stanton-Reid <stantonr@bu.edu>
2017-11-09 23:29:02 -0800

    Create user factory

---
+++ user_factory.py
@@ -0,0 +1,19 @@
+import uuid
+import endpoints
+from protorpc import *
+
+from models import *
+
+def create_new_user(username, password):
+    try:
+        userid = str(uuid.uuid4())
+        new_prof = UserProfile(id=userid, username=username, userid=userid)
+        cred = Credential()
+        cred.hash_password(password)
+        cred.set_token()
+        cred.put()
+        new_prof.credential = cred.key
+        new_prof.put()
+        return new_prof
+    except:
+        raise endpoints.BadRequestException("Google Datastore save error.")
